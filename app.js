b3a730b2c69fc63fda58223495699c678d55c873
JaeIn <sm810403@hotmail.com>
2020-06-28 00:47:21 -0700

    3rd commit
    jumping applied

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const hole = document.querySelector('.hole');
 const char = document.querySelector('.character');
 
+//hole random height
 hole.addEventListener('animationiteration', ()=>{
     let random = ((Math.random()*150)+100);
     hole.style.top = random + 'px';
 });
 
+//gravity
 let count = 0;
 setInterval(()=>{
     count += 1;
@@ -15,3 +17,23 @@ setInterval(()=>{
         count=140;
     }
 },10)
+
+//jump
+window.addEventListener('keydown', (e)=>{
+    if (e.keyCode===32) {
+        count -= 60;
+        char.style.top =count+'px';    
+    }
+    
+})
+//score
+let score = 0;
+let scores = document.querySelector('.score');
+
+function getScore() {
+    if (block.style.left === -300) {
+        score +=10;
+        scores.innerHTML = score;
+    }
+}
+          
